cf728fbf80562afddd567e8453a476b4a5ed2bb7
Nicholas Lagerstedt <nicholaslagerstedt@nicholass-macbook-pro.local>
2016-03-25 19:32:05 -0400

    Initial Commit

---
+++ Shapes on a FloorTests/Shapes_on_a_FloorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shapes_on_a_FloorTests.swift
+//  Shapes on a FloorTests
+//
+//  Created by Nicholas Lagerstedt on 3/25/16.
+//  Copyright © 2016 Nicholas Lagerstedt. All rights reserved.
+//
+
+import XCTest
+@testable import Shapes_on_a_Floor
+
+class Shapes_on_a_FloorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
